b4baed576817da84f0bfd20396671353ba4f8fcd
hajer <hajer.blohorn@gmail.com>
2025-09-05 10:13:44 +0200

    app sql request pc_collegue

--- app.py
+++ app.py
@@ -1,23 +1,63 @@
 import streamlit as st
 import pandas as pd
 import duckdb
+import io
 
-st.write("""SQL SRS 
-Spaced Repetition System SQL practice""")
+csv1 = '''
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
+'''
 
-options = st.selectbox(
-    "What would you like to review?",
-    ["joins", "GroupBy", "Window Functions"],
-    index = None,
-    placeholder = "Select a theme ...",
-)
+beverages = pd.read_csv(io.StringIO(csv1))
 
-st.write('you selected:', options)
+csv2 = '''
+food_item,food_price
+cookie juice,2.5
+chocolatine,2
+muffin,3
+'''
 
-df = pd.read_csv("data/raw/EdStatsCountry.csv")
-st.dataframe(df)
+food_items = pd.read_csv(io.StringIO(csv2))
 
-sql_query = st.text_area(label="Entrez votre query:")
-result = duckdb.query(sql_query).df()
-st.write(f"Vous avez entré la query suivante: {sql_query}")
-st.dataframe(result)
+answer = """
+SELECT * FROM beverages
+"""
+
+solution = duckdb.sql(answer).df()
+st.header("Entrez votre code:")
+query = st.text_input(label="Votre code SQL ici", key="user_input")
+
+if query:
+    result = duckdb.sql(query).df()
+    st.dataframe(result)
+
+tab2, tab3 = st.tabs(["Tables", "Solution"])
+
+with tab2:
+    st.write("table: beverages")
+    st.dataframe(beverages)
+    st.write("table: food_items")
+    st.dataframe(food_items)
+    st.write("expected:")
+    st.dataframe(solution)
+with tab3:
+    st.write(answer)
+
+#options = st.selectbox(
+#    "What would you like to review?",
+#    ["joins", "GroupBy", "Window Functions"],
+#    index = None,
+#    placeholder = "Select a theme ...",
+#)
+
+#st.write('you selected:', options)
+
+#df = pd.read_csv("data/raw/EdStatsCountry.csv")
+#st.dataframe(df)
+
+#sql_query = st.text_area(label="Entrez votre query:")
+#result = duckdb.query(sql_query).df()
+#st.write(f"Vous avez entré la query suivante: {sql_query}")
+#st.dataframe(result)
