9b30751e4252e8cb177d4454811dabfca54af59d
hajer <hajer.blohorn@gmail.com>
2025-09-12 18:39:04 +0200

    add users input to display specifics exercise

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 # pylint: disable=missing-module-docstring
+import ast
+
 import duckdb
 import streamlit as st
 
@@ -20,28 +22,22 @@ with st.sidebar:
 st.header("Entrez votre code:")
 query = st.text_input(label="Votre code SQL ici", key="user_input")
 
+if query:
+    result = con.execute(query).df()
+    st.dataframe(result)
 
-# if query:
-#     result = duckdb.sql(query).df()
-#     st.dataframe(result)
-#     try:
-#         result = result[solution_df.columns]
-#         st.dataframe(result.compare(solution_df))
-#     except KeyError as e:
-#         st.write("Some columns are missing")
-#
-#     n_lines_difference = result.shape[0] - solution_df.shape[0]
-#     if n_lines_difference != 0:
-#         st.write(f"{n_lines_difference} lines difference with the solution_df")
-#
-# tab2, tab3 = st.tabs(["Tables", "Solution"])
-#
-# with tab2:
-#     st.write("table: beverages")
-#     st.dataframe(beverages)
-#     st.write("table: food_items")
-#     st.dataframe(food_items)
-#     st.write("expected:")
-#
-# with tab3:
-#     st.write(ANSWER_STR)
+tab2, tab3 = st.tabs(["Tables", "Solution"])
+
+with tab2:
+    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+    for table in exercise_tables:
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(df_table)
+
+with tab3:
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+    st.write(answer)
+
